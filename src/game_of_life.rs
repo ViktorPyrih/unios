4428b306c0e307e69be3674fca07b2b283cd178d
ViktorPyrih <v24102002>
2022-12-17 18:48:07 +0200

    Implemented vga_buffer driver and game_of_life algorithm

--- src/game_of_life.rs
+++ src/game_of_life.rs
@@ -1,5 +1,6 @@
-use crate::Screen;
-use crate::vga_buf::AsciiChar;
+use crate::vga_buffer::{AsciiChar, Screen};
+use core::fmt::Write;
+use core::ptr::write;
 
 const MAP: [&str; 25] = [
     "                                                                                ",
@@ -29,7 +30,10 @@ const MAP: [&str; 25] = [
     "                                                                                "
 ];
 
-pub fn game_of_life(screen: &mut Screen) {
+const LIVE_CELL: u8 = b'x';
+const DEAD_CELL: u8 = b' ';
+
+pub fn game_of_life(screen: &mut Screen) -> ! {
     let mut current_gen: [[u8; 80]; 25] = [[0; 80]; 25];
     for i in 0..MAP.len() {
         for (j, byte) in MAP[i].bytes().enumerate() {
@@ -37,5 +41,70 @@ pub fn game_of_life(screen: &mut Screen) {
         }
     }
 
-    // TODO: implement game of life
+    screen.write_arr(&current_gen);
+
+    loop {
+        current_gen = get_next_gen(&current_gen);
+        screen.write_arr(&current_gen);
+        wait();
+    }
+}
+
+fn get_next_gen(gen: &[[u8; 80]; 25]) -> [[u8; 80]; 25] {
+
+    let mut next_gen: [[u8; 80]; 25] = [[0; 80]; 25];
+
+    for i in 0..25 {
+        for j in 0..80 {
+            let neighbours = get_neighbours_count(&gen, &i, &j);
+            let cell_type = gen[i as usize][j as usize];
+
+            if cell_type == LIVE_CELL && (neighbours == 2 || neighbours == 3){
+                next_gen[i as usize][j as usize] = LIVE_CELL;
+            }
+            else if cell_type == DEAD_CELL && neighbours == 3{
+                next_gen[i as usize][j as usize] = LIVE_CELL;
+            }
+            else{
+                next_gen[i as usize][j as usize] = DEAD_CELL;
+            }
+        }
+    }
+
+    return next_gen;
+}
+
+fn get_neighbours_count(gen: &[[u8; 80]; 25], i: &usize, j: &usize) -> i32 {
+    let mut count : i32 = 0;
+
+    if i > &0 && gen[i - 1][*j] == LIVE_CELL{
+        count += 1;
+    }
+    if i > &0 && j > &0 && gen[i - 1][j - 1] == LIVE_CELL{
+        count += 1;
+    }
+    if i > &0 && j + 1 < 80 && gen[i - 1][j + 1] == LIVE_CELL{
+        count += 1;
+    }
+    if i + 1 < 25 && gen[i + 1][*j] == LIVE_CELL{
+        count += 1;
+    }
+    if i + 1 < 25 && j > &0 && gen[i + 1][j - 1] == LIVE_CELL{
+        count += 1;
+    }
+    if i + 1 < 25 && j + 1 < 80 && gen[i + 1][j + 1] == LIVE_CELL{
+        count += 1;
+    }
+    if j > &0 && gen[*i][j - 1] == LIVE_CELL{
+        count += 1;
+    }
+    if j + 1 < 80 && gen[*i][j + 1] == LIVE_CELL{
+        count += 1;
+    }
+
+    return count;
+}
+
+fn wait() {
+    for i in 0..100000 {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-mod vga_buf;
+mod vga_buffer;
 mod game_of_life;
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::ptr::write;
 use crate::game_of_life::game_of_life;
-use crate::vga_buf::{Alignment, DEFAULT_COLOR, Screen};
+use crate::vga_buffer::{Alignment, Color, Screen};
 
 /// This function is called on panic.
 #[panic_handler]
@@ -18,9 +18,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let mut screen = Screen::new(DEFAULT_COLOR, Alignment::Left);
-
-    screen.print_hello_world();
-
+    /*
+    let mut screen = Screen::new(Color::Yellow, Color::Blue, Alignment::Center);
+    for i in 0..100 {
+        write!(screen, "Number {}\n", i);
+    }
     loop {}
+    */
+
+    let mut screen = Screen::new(Color::LightGreen, Color::Black, Alignment::Left);
+    game_of_life(&mut screen);
+
 }
